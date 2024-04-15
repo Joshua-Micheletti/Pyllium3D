b5ec74784c2bf1f3b74f1492d29d830c79e419cc
j.micheletti <j.micheletti@ebw.it>
2024-04-15 10:17:46 +0200

    removed physics

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -1,6 +1,6 @@
 from utils.singleton import Singleton
 from renderer.renderer_manager import RendererManager
-from physics.physics_world import PhysicsWorld
+# from physics.physics_world import PhysicsWorld
 
 
 class Engine(metaclass=Singleton):
@@ -12,9 +12,9 @@ class Engine(metaclass=Singleton):
 
     def update(self):
         rm = RendererManager()
-        pw = PhysicsWorld()
+        # pw = PhysicsWorld()
 
-        for physics_body, model in self.links.items():
-            position, rotation = pw.get_position_rotation(physics_body)
-            rm.place(model, *position)
-            rm.rotate(model, *rotation)
+        # for physics_body, model in self.links.items():
+        #     position, rotation = pw.get_position_rotation(physics_body)
+        #     rm.place(model, *position)
+        #     rm.rotate(model, *rotation)
